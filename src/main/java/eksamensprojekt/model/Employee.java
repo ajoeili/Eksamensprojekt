f9ccd60ccc88583b9f3cd1c117b708210620a2ef
Ajo <ajsy0001@stud.kea.dk>
2024-12-03 14:20:58 +0100

    Updates to initial commit

---
+++ src/main/java/eksamensprojekt/model/Employee.java
@@ -0,0 +1,79 @@
+package eksamensprojekt.model;
+
+public class Employee {
+    private int employeeId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String role;
+    private boolean isProjectManager;
+
+    public Employee() {}
+
+    public Employee(int employeeId, String firstName, String lastName, String email, String password, String role, boolean isProjectManager) {
+        this.employeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.isProjectManager = isProjectManager;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public boolean isProjectManager() {
+        return isProjectManager;
+    }
+
+    public void setProjectManager(boolean projectManager) {
+        isProjectManager = projectManager;
+    }
+}
---
+++ src/main/java/eksamensprojekt/model/Project.java
@@ -0,0 +1,71 @@
+package eksamensprojekt.model;
+
+import java.util.Date;
+import java.util.List;
+
+public class Project {
+    private int projectId;
+    private String name;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+    private List<Employee> employees;
+
+    public Project() {}
+
+    public Project(int projectId, String name, String description, Date startDate, Date endDate) {
+        this.projectId = projectId;
+        this.name = name;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public int getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(int projectId) {
+        this.projectId = projectId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+}
